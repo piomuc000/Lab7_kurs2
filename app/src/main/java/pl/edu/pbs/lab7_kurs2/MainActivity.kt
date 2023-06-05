d2e0236819c107fcffd2f4e4c4b1893aea019d13
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-06-05 22:00:33 +0200

    Initial commit

---
+++ app/src/main/java/pl/edu/pbs/lab7_kurs2/MainActivity.kt
@@ -0,0 +1,46 @@
+package pl.edu.pbs.lab7_kurs2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pl.edu.pbs.lab7_kurs2.ui.theme.Lab7_kurs2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab7_kurs2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab7_kurs2Theme {
+        Greeting("Android")
+    }
+}

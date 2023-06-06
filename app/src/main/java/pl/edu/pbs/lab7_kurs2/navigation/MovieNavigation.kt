9b1f03daaa0fd77e702c2d0ab80b9de5ee8190ce
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-06-06 18:44:57 +0200

    Działa!

--- app/src/main/java/pl/edu/pbs/lab7_kurs2/navigation/MovieNavigation.kt
+++ app/src/main/java/pl/edu/pbs/lab7_kurs2/navigation/MovieNavigation.kt
@@ -2,14 +2,17 @@ package pl.edu.pbs.lab7_kurs2.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import pl.edu.pbs.lab7_kurs2.screens.home.HomeScreen
 
 @Preview
 @Composable
 fun MovieNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = MovieScreens.HomeScreen.name){
-        composable(MovieScreens.HomeScreen.name){
+    NavHost(navController = navController, startDestination = MovieScreens.HomeScreen.name) {
+        composable(MovieScreens.HomeScreen.name) {
             HomeScreen(navController = navController)
         }
     }
--- app/src/main/java/pl/edu/pbs/lab7_kurs2/screens/home/HomeScreens.kt
+++ app/src/main/java/pl/edu/pbs/lab7_kurs2/screens/home/HomeScreens.kt
@@ -2,19 +2,19 @@ package pl.edu.pbs.lab7_kurs2.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import pl.edu.pbs.lab7_kurs2.MovieRow
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import pl.edu.pbs.lab7_kurs2.navigation.MovieScreens
+import pl.edu.pbs.lab7_kurs2.MovieRow
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
     Scaffold(
@@ -22,32 +22,34 @@ fun HomeScreen(navController: NavController) {
             TopAppBar(backgroundColor = Color.Magenta, elevation = 5.dp) {
                 Text(text = "Movies TopAppBar")
             }
-        }
-    ) {
-            it->Column(modifier = Modifier.padding(it)) {
+        },
+        content = {
+            Column(modifier = Modifier.padding(it)) {
                 MainContent(navController = navController)
-        }
-    }
-}
-
-@Composable
-fun MainContent(navController: NavController,
-                moviesList: List<String> = listOf(
-                    "Avatar",
-                    "555",
-                    "Harry Potter",
-                    "Life",
-                    "Lolek",
-                    "Bolek",
-                    "Krecik"
-                )
-) {
-    Column(modifier = Modifier.padding(12.dp)) {
-        LazyColumn {
-            items(items = moviesList) {
-                MovieRow(movie = it) { movie -> navController.navigate(route = MovieScreens.DetailsScreen.name+"/$movie")}
             }
         }
+    )
+}
 
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    moviesList: List<String> = listOf(
+        "Avatar",
+        "555",
+        "Harry Potter",
+        "Life",
+        "Lolek",
+        "Bolek",
+        "Krecik"
+    )
+) {
+    LazyColumn {
+        items(items = moviesList) { movie ->
+            MovieRow(movie = movie) { movie2 ->
+                navController.navigate(route = "${MovieScreens.DetailsScreen.name}/$movie2")
+            }
+        }
     }
 }
